e2708ce91f36ace0a8f4b69670eb60552aa94e27
Yannick Pfeiffer <yannick_pfeiffer@yahoo.com>
2023-04-27 21:41:09 +0200

    Additional setup: primary color, prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#276a78',
+					50: '#f4f8f8',
+					100: '#e9f0f2',
+					200: '#c9dadd',
+					300: '#a9c3c9',
+					400: '#6897a1',
+					500: '#276a78',
+					600: '#235f6c',
+					700: '#1d505a',
+					800: '#174048',
+					900: '#13343b'
+				}
+			}
+		}
 	},
 	plugins: []
 };
