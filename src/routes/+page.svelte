35178cb0a0a41680ee9115155104c8ba7471f3a5
Yannick Pfeiffer <yannick_pfeiffer@yahoo.com>
2023-04-27 21:41:15 +0200

    Fix login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,73 +2,31 @@
 	import logo from '$lib/images/logo.svg';
 </script>
 
-<div class="container">
-	<button class="signUpButton">Sign Up</button>
+<div class="w-screen h-screen flex items-center justify-center flex-col pb-20">
+	<button
+		class="absolute px-6 py-2 rounded-sm bg-primary hover:bg-primary-400 text-white right-8 top-8"
+		>Sign Up</button
+	>
 	<img alt="The project logo" src={logo} />
-	<div class="innerContainer">
-		<div class="inputContainer">
+	<div class="py-2 mb-2 mt-10 gap-2 flex flex-col">
+		<div class="flex flex-col">
 			<label class="" for="">Email</label>
-			<input name="email" type="email" />
+			<input
+				class="outline-primary border-gray border-2 rounded-sm px-3 py-2 w-72"
+				name="email"
+				type="email"
+			/>
 		</div>
-		<div class="inputContainer">
+		<div class="flex flex-col">
 			<label for="">Password</label>
-			<input name="password" type="password" />
+			<input
+				class="outline-primary border-gray border-2 rounded-sm px-3 py-2 w-72"
+				type="password"
+			/>
 		</div>
 	</div>
-	<button class="signInButton">Sign In</button>
+	<button class="px-6 rounded-sm bg-black hover:bg-opacity-80 text-white py-2">Sign In</button>
+	<button class="px-6 rounded-sm text-black border border-gray py-2 mt-8 hover:bg-slate-100">
+		Sign In with Google
+	</button>
 </div>
-
-<style>
-	.container {
-		background: white;
-		width: 100vw;
-		height: 100vh;
-		color: black;
-		text-align: center;
-		align-items: center;
-		justify-content: center;
-		display: flex;
-		flex-direction: column;
-		padding-bottom: 40px;
-	}
-	.innerContainer {
-		margin-top: 40px;
-		margin-bottom: 16px;
-		gap: 16px;
-		display: flex;
-		flex-direction: column;
-	}
-	.inputContainer {
-		display: flex;
-		flex-direction: column;
-		text-align: left;
-		width: 300px;
-	}
-	.signInButton {
-		padding: 12px 32px;
-		background-color: black;
-		outline: none;
-		border: none;
-		color: white;
-		border-radius: 2px;
-	}
-	.signUpButton {
-		position: absolute;
-		top: 32px;
-		right: 32px;
-		padding: 12px 32px;
-		background-color: #276a78;
-		outline: none;
-		border: none;
-		color: white;
-		border-radius: 2px;
-	}
-	input {
-		padding: 12px 8px;
-		outline-color: #276a78;
-		border-color: #dbdbdb;
-	}
-	img {
-		width: 550px;
-	}
-</style>
